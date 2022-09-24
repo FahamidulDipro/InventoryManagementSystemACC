ef7d2efce5b456610448482fcb737266afa96abd
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-09-24 23:19:15 +0600

    Brand schema added

---
+++ models/Brand.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const { ObjectID } = mongoose.Schema.Types;
+const brandSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a brand name"],
+      maxLength: 100,
+      unique: true,
+      lowercase: true,
+    },
+    description: String,
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Please provide a valid email"],
+      lowercase: true,
+    },
+    website: {
+      type: String,
+      validate: [validator.isURL, "Plese provide a valid URL"],
+    },
+    location: String,
+    products: [
+      {
+        type: ObjectID,
+        ref: "Product",
+      },
+    ],
+    supplier: [
+      {
+        name: String,
+        contactNumber: String,
+        id: {
+          type: ObjectID,
+          ref: "Supplier",
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+  },
+  { timestamps: true }
+);
+
+exports.Brand = mongoose.model("Brand", brandSchema);
